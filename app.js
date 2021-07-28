b984834e2fa584640b56b1558a9d755d229df1ec
spreet247 <samre2472@gmail.com>
2021-07-28 18:12:53 -0400

    grid

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 const Container = document.querySelector(".container");
 
-for (let i = 0; i <= 256; i++) {
+for (let i = 0; i <= 255; i++) {
   const Div = document.createElement("div");
-  Div.style.height = "16px";
-  Div.style.width = "16px";
   Div.style.backgroundColor = "gray";
   Div.style.color = "white";
   Div.style.border = "1px solid black";

998a210043c208e99a91bd6ec0f552340d373af2
spreet247 <samre2472@gmail.com>
2021-07-30 21:21:33 -0400

    multiColor

--- app.js
+++ app.js
@@ -8,6 +8,15 @@ for (let i = 0; i <= 255; i++) {
 
   Container.appendChild(Div);
   Div.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = "white";
+    e.target.style.backgroundColor = get_random_hex_color();
   });
+
+  function get_random_hex_color() {
+    var letters = "0123456789ABCDEF";
+    var color = "#";
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
 }

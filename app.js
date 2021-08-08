6af8c70e470c888022fb68458b6fcfa562579add
spreet247 <samre2472@gmail.com>
2021-08-08 14:27:13 -0400

    done

--- app.js
+++ app.js
@@ -1,32 +1,29 @@
 const Container = document.querySelector(".container");
 const Button = document.querySelector("button");
+let gridNum = prompt("Please enter a positive number");
 
-for (let i = 0; i <= 255; i++) {
-  const Div = document.createElement("div");
-  Div.style.backgroundColor = "gray";
-  Div.style.color = "white";
-  Div.style.border = "1px solid black";
+for (let i = 0; i <= gridNum; i++) {
+  for (let j = 0; j <= gridNum; j++) {
+    const Div = document.createElement("div");
+    Div.style.backgroundColor = "gray";
+    Div.style.color = "white";
+    Div.style.border = "1px solid black";
 
-  Container.appendChild(Div);
-  Div.addEventListener("mouseover", (e) => {
-    e.target.style.backgroundColor = get_random_hex_color();
-  });
-  get_random_hex_color();
-  Button.addEventListener("click", (e) => {
-    reset();
-  });
+    Container.appendChild(Div);
+    Div.addEventListener("mouseover", (e) => {
+      e.target.style.backgroundColor = get_random_hex_color();
+    });
+    get_random_hex_color();
+    Button.addEventListener("click", (e) => {
+      reset();
+    });
+  }
 }
 
 function reset() {
   window.location.reload();
 }
 
-function grid() {
-  let gridNum = prompt("Please enter a positive number");
-  while (gridNum <= 0) {
-    return gridNum;
-  }
-}
 function get_random_hex_color() {
   var letters = "0123456789ABCDEF";
   var color = "#";
